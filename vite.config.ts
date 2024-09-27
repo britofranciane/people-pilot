07bc36f957930cedc048303abeb830aa82c680e9
franciane-brito <francianebrito99@gmail.com>
2024-09-26 23:02:50 -0300

    feat: adjust import types

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@services': path.resolve(__dirname, './src/services'),
+      types: path.resolve(__dirname, './src/types'),
     },
   },
 });
